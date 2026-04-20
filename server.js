701aa9416f6a2f9d1989878c0f32fafad3667008
AbhiDevOps369 <abhiram.ghotkar11@gmail.com>
2026-04-20 17:39:04 +0530

    Update server.js made sure server is listening to "0.0.0.0"

--- server.js
+++ server.js
@@ -124,6 +124,6 @@ app.delete('/api/students/:id', (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0",() => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
